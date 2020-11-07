dfd7283aba05f00ce88a9f6d706a612710b5f28d
Zheng Andrew <andrewzheng04@gmail.com>
2020-11-07 14:36:03 -0800

    removed header and addedl ogout

--- App.js
+++ App.js
@@ -1,6 +1,6 @@
 import 'react-native-gesture-handler';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 
@@ -15,11 +15,14 @@ export default class App extends React.Component {
   render () {
     return(
       <NavigationContainer>
-        <Stack.Navigator>
+        <Stack.Navigator screenOptions={{
+                headerShown: false
+              }}>
           <Stack.Screen name="SplashScreen" component={SplashScreen} />
           <Stack.Screen name="LoginScreen" component={LoginScreen} />
           <Stack.Screen name="RegisterScreen" component={RegisterScreen} />
-          <Stack.Screen name="HomeScreen" component={HomeScreen} />
+          <Stack.Screen name="HomeScreen" component={HomeScreen} 
+            />
         </Stack.Navigator>
       </NavigationContainer>
     );
--- components/HomeScreen.js
+++ components/HomeScreen.js
@@ -1,18 +1,40 @@
 import React from 'react';
-import { StyleSheet, Text, View, Button } from 'react-native';
+import { StyleSheet, Text, View, Button, Dimensions } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
+import  MapView, { Marker } from 'react-native-maps';
 
 export default class HomeScreen extends React.Component {
+    logout = () =>{
+        this.props.navigation.navigate('LoginScreen')
+    }
     render () {
         return(
             <View style={styles.container}>
-                <Text>home</Text>
-                    <Button title="logout" 
-                    onPress={() =>
-                        this.props.navigation.navigate('LoginScreen')
-                    }
+                
+
+            <MapView style={styles.mapStyle}
+                initialRegion={{
+                latitude: 51.5078788,
+                longitude: -0.0877321,
+                latitudeDelta: 0.009,
+                longitudeDelta: 0.009
+            }}>
+                <Marker
+                    coordinate=
+                    {{
+                        latitude: 51.5078788,
+                        longitude: -0.0877321
+                    }}
+                    title={'Title'}
+                    description={'Describe this is cool'}
                 />
+            </MapView>
+            <View style={styles.logout}>
+                        <Button title="logout" 
+                        onPress={this.logout}
+                    />
+                </View>
             </View>
           );
     }
@@ -22,7 +44,17 @@ const styles = StyleSheet.create({
     container: {
       flex: 1,
       backgroundColor: '#fff',
-      alignItems: 'center',
-      justifyContent: 'center',
+    //   alignItems: 'center',
+    //   justifyContent: 'center',
     },
+    mapStyle: {
+      flex: 1,
+      width: Dimensions.get('window').width,
+      height: Dimensions.get('window').height,
+    },
+
+    logout:{
+        position: 'absolute',
+        alignSelf:'flex-end',
+    }
   });

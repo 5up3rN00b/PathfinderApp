508e16d6272c34f3417c793de2f825d05b275a52
larryz1230 <larryzhi1230@gmail.com>
2020-11-07 20:47:28 -0800

    list do move!

--- components/LoginScreen.js
+++ components/LoginScreen.js
@@ -1,9 +1,50 @@
 import React from 'react';
-import { StyleSheet, Text, View, Button } from 'react-native';
+import { StyleSheet, Text, View, Button, TouchableOpacity } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
+import DraggableFlatList from "react-native-draggable-flatlist";
+
+const exampleData = [...Array(5)].map((d, index) => ({
+    key: `item-${index}`, // For example only -- don't use index as your key!
+    label: index,
+    backgroundColor: "red",
+  }));
+
+  
 
 export default class LoginScreen extends React.Component {
+
+    state = {
+        data : exampleData,
+    }
+
+
+    renderItem = ({ item, index, drag, isActive }) => {
+        return (
+          <TouchableOpacity
+            style={{
+              height: 100,
+              marginVertical: 10,
+              backgroundColor: isActive ? "blue" : item.backgroundColor,
+              alignItems: "center",
+              justifyContent: "center"
+            }}
+            onLongPress={drag}
+          >
+            <Text
+              style={{
+                fontWeight: "bold",
+                color: "white",
+                fontSize: 32
+              }}
+            >
+              {item.label}
+              {}
+            </Text>
+          </TouchableOpacity>
+        );
+      };
+
     render () {
         return(
             <View style={styles.container}>
@@ -14,8 +55,19 @@ export default class LoginScreen extends React.Component {
                     }
                 />
             </View>
+
+        //     <View style={{ flex: 1 }}>
+        // <DraggableFlatList
+        //   data={this.state.data}
+        //   renderItem={this.renderItem}
+        //   keyExtractor={(item, index) => `draggable-item-${item.key}`}
+        //   onDragEnd={({ data }) => this.setState({ data })}
+        // />
+        // </View>
           );
     }
+
+    
 }
 
 const styles = StyleSheet.create({
